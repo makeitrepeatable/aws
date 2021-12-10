f904cd0c03b96169fb0d04c85126fed0b23312bf
Scott Collins <scott.collins@pollinate.co.uk>
2021-12-10 16:45:05 +0000

    initial commit

---
+++ checkout/s3.tf
@@ -0,0 +1,51 @@
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = var.s3_bucket_name
+  acl    = var.s3_acl
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_object" "checkout" {
+  bucket = aws_s3_bucket.demo_bucket.bucket
+  key    = "checkout.png"
+  source = "images/checkout.png"
+  acl    = var.s3_acl
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.demo_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.demo_bucket.id
+
+    custom_origin_config {
+      http_port              = var.cf_custom_origin_config.http_port
+      https_port             = var.cf_custom_origin_config.https_port
+      origin_protocol_policy = var.cf_custom_origin_config.origin_protocol_policy
+      origin_ssl_protocols   = var.cf_protocols
+    }
+  }
+  enabled         = true
+  is_ipv6_enabled = true
+  default_cache_behavior {
+    allowed_methods  = var.cf_allowed_methods
+    cached_methods   = var.cf_cached_methods
+    target_origin_id = aws_s3_bucket.demo_bucket.id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+  }
+  price_class = "PriceClass_200"
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  depends_on = [aws_s3_bucket.demo_bucket]
+}
---
+++ checkout/tests/ec2_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"fmt"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAwsAsg(t *testing.T) {
+	t.Parallel()
+
+	// create a random asg name so that we can differentiate test resources from real ones
+	expectedName := fmt.Sprintf("terratest-asg-%s", random.UniqueId())
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+
+		Vars: map[string]interface{}{
+			"asg_name": expectedName,
+		},
+	})
+
+	// run destroy to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// run init and apply and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// get the value of an output variable
+	instanceID := terraform.Output(t, terraformOptions, "elb_dns_name")
+
+	// use tf output to validate that resources were created siuccessfully
+	assert.NotEmpty(t, instanceID)
+
+}
