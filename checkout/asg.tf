588bdb3d1fa4047399ee4c16938c2a3e2a26059f
Scott Collins <scott.collins@pollinate.co.uk>
2021-12-12 14:59:07 +0000

    forces random cars to lowercase for s3

--- checkout/asg.tf
+++ checkout/asg.tf
@@ -2,6 +2,7 @@ resource "random_string" "random" {
   length      = 5
   special     = false
   min_numeric = 2
+  min_lower   = 5
 }
 resource "aws_security_group" "allow_http_ssh" {
   name        = "allow_inbound"
